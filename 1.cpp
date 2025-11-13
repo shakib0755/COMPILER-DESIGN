1eee909d569122c8f75a647d346c8fa24be7fac1
shakib0755 <138766692+shakib0755@users.noreply.github.com>
2025-11-13 12:12:13 +0600

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,32 +1,15 @@
-/*
 #include <iostream>
 using namespace std;
 int main() {
-    int arr[] = {10, 20, 30, 40, 50};
-    int n=5;
-    int sum=0;
-    for(int i = 0; i <n; i++) {
-        sum=sum+arr[i];
-    }
-    float avg = (float)sum/n;
-    cout << "Average = " << avg << endl;
+    string a="a", b="b", c="c", d="d", e="e", f="f";
+    //a = b * c + d / e - f
+    cout << "A = " << b << " * " << c << endl;
+    cout << "B = " << d << " / " << e << endl;
+    cout << "C =A + B" << endl;
+    cout << a << " = C - " << f << endl;
     return 0;
 }
-*/
-#include <iostream>
-using namespace std;
-int main() {
-    int n;
-    cout<<"enter total number: ";
-    cin>>n;
-    int arr[n];
-    int sum=0;
-    for(int i = 0; i<n; i++) {
-            cin>>arr[i];
-        sum=sum+arr[i];
-    }
-    float avg = (float)sum/n;
-    cout << "Average = " << avg << endl;
-    return 0;
-}
+
+
+
 
